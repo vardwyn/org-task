d0430a1181674938c4b450315af66b2d0386d3a3
vardwyn <vardwyn@vardwyn.com>
2026-02-19 18:37:38 +0100

    license: Add licensing info

--- org-task.el
+++ org-task.el
@@ -4,9 +4,29 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29.1") (org "9.7"))
 ;; Keywords: outlines, convenience
-
+;;
+;; URL: https://github.com/vardwyn/org-task
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; Copyright (C) 2026 Vardwyn
+;;
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License version 3
+;; as published by the Free Software Foundation
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;
 ;;; Commentary:
-
+;;
 ;; org-task provides deterministic weighted sampling for Org tasks.
 ;; It exposes K/T sampling helpers and agenda integration helpers.
 
